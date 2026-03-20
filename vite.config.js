9e2dbe5dfcf7531e07cb193e671c34e7ecde5e18
Chikadibia Anthony <chikadibia86@yahoo.com>
2026-03-20 07:57:35 +0100

    fuck you

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: true,
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      protocol: 'wss', // Force secure WebSocket for ngrok
+      clientPort: 443,
+    },
+  },
 })
